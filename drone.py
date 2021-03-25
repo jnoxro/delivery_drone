7875955edfcce313ec2f3398950ce367bcfa5d21
jnoxro <jackorton32@gmail.com>
2021-03-25 21:35:48 +0000

    setup gsm

--- drone.py
+++ drone.py
@@ -1,5 +1,6 @@
 
 import time
+#from time import time
 import sys
 import serial
 import spidev
@@ -36,7 +37,7 @@ def setup_pins():
 
 
 def buff_check(uart): #check spi2uart module for received data in buffer on specific uart
-	print("hell")
+
 	send2 = [0x10 | uart] #0x10 = read number of bytes in receive buffer
 
 	gpio.output(ss1, gpio.LOW) #begin spi communication
@@ -47,20 +48,46 @@ def buff_check(uart): #check spi2uart module for received data in buffer on spec
 	return num #return buffer info
 
 
-def setup_gsm():
+def setup_gsm(): #check connection to and set up the gsm module
 
 	print("GSM Setup")
-	msg = "AT\n"
-	msg = list(bytearray(msg.encode()))
+	stage = 0 #track setup stage
+	time1 = time.time() #timers for time out / retrying
+	time2 = time.time()
 
-	gpio.output(ss1, gpio.LOW)
-	msgi = [0x40]
-	spi.xfer(msgi)
-	msgl = [len(msg)]
-	spi.xfer(msgl)
+	while stage < 10: #while stages left to go
 
-	spi.xfer(msg)
-	gpio.output(ss1, gpio.HIGH)
+		if stage == 0: #Check module is connected (AT should reply with 'OK')
+			print("send AT")
+			msg = "AT\n"
+			msg = list(bytearray(msg.encode())) #convert message into sendable data
+
+			gpio.output(ss1, gpio.LOW) #begin communication with spi2uart
+			msgi = [0x40] #0x40 = send data over uart0
+			spi.xfer(msgi)
+			msgl = [len(msg)] #this might be risky code: if len>9, no longer encoded properly
+			spi.xfer(msgl)
+			spi.xfer(msg) #send message
+			gpio.output(ss1, gpio.HIGH) #end communication
+
+			time1 = time.time() #record time AT sent for timeout
+			stage = 1 #move onto next stage
+
+		if stage == 1: #listen for response from gsm module (we expect 'OK')
+			buff = buff_check(0x00) #check if data is received on uart0 buffer of spi2uart
+			if buff[0] > 0: #if buffer has bytes
+				print("Response detected")
+				stage = 2 #move to read buffer
+			else:
+				time.sleep(1) #else wait for response
+
+			if time.time()-time1 > 10: #if no response in 10 seconds, return to stage 0 and resend AT
+				print("timeout, retrying AT")
+				stage = 0
+
+		if stage == 2: #read response from module, ensure it is 'OK', otherwise retry
+			print("stage 2, nice wan")
+			time.sleep(2)
 
 def setup_lora():
 	print("Hi")
